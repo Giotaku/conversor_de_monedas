535cc9c540f45e26803c069a9942d36aebcc27ad
Juan Giovanny Rosas Ledesma <giovannyledesma99@yahoo.com>
2022-03-06 00:47:18 -0600

    primer version de conversor de monedas

---
+++ conversor.py
@@ -0,0 +1,12 @@
+def main():
+    pesos = input("Cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
+    
+if __name__ == "__main__":
+    main()
